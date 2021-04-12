7911152f451c08ac3075a30668f95eec6b0c6155
wangdi <1589955459@qq.com>
2021-04-12 17:20:31 +0800

    bug0

--- src/main/java/com/example/springboottest/git/LearnGit.java
+++ src/main/java/com/example/springboottest/git/LearnGit.java
@@ -3,6 +3,7 @@ package com.example.springboottest.git;
 public class LearnGit {
     public void add(){
         int x = 10;
-        int y = 10 / 0;
+        int y = 10 / x;
     }
+    public void fix(){}
 }
